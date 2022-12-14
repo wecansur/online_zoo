d3562cfbca07c3bc393a7f15d4791fc2315915bb
wecansur <vlkob95@gmail.com>
2022-12-14 13:17:32 +0200

    feat: pugin html and sass to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,53 @@
-let path = require('path');
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-let conf = {
+module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'main.js'
-    }
+        filename: 'main.js',
+        publicPath: 'auto'
+    },
+    mode: 'development',
+    devServer: {
+        compress: true,
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {minimize: true},
+                    }
+                ]
+            }, {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates 'style' nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                    {
+                        loader: 'sass-resources-loader',
+                        options: {
+                            resources: [
+                                'src/styles/vars.scss',
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
 };
-
-module.exports = conf;
